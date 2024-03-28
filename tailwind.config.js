000b0bb6fc209482d2c8db6f6595f280045917f1
Preciousglows <worukwoprecious@gmail.com>
2024-03-28 13:37:37 +0100

    Edited menu bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,17 @@ module.exports = {
 
       animation:{
         marquee: 'marquee 20s linear infinite'
-      }
+      },
+
+      backgroundImage: {
+        financeTeam: "linear-gradient(0deg, rgba(0,0,0, 0.5) 11.4%, rgba(0, 0, 0, 0.5) 70.2%), url('./assets/finance-team.svg')",
+        legalTeam: "linear-gradient(0deg, rgba(0,0,0, 0.5) 11.4%, rgba(0, 0, 0, 0.5) 70.2%), url('./assets/legal-team.svg')", 
+        salesTeam: "linear-gradient(0deg, rgba(0,0,0, 0.5) 11.4%, rgba(0, 0, 0, 0.5) 70.2%), url('./assets/sales-team.svg')",
+
+     }
+
+
+     
     },
   },
   plugins: [],
